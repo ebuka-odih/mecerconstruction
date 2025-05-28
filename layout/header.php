3ce2c46556a1fad3eaa369a51477c582bb284b3b
Gnosis <emmagab38@gmail.com>
2025-05-28 07:10:38 +0100

    commit changes

--- layout/header.php
+++ layout/header.php
@@ -54,12 +54,13 @@
                 <div class="row align-items-center justify-content-center">
                     <div class="col-md-4">
                         <div class="header-left">
-                            <a href="https://www.facebook.com/login/" target="_blank"><i class='bx bxl-facebook'></i></a>
-                            <a href="https://twitter.com/login" target="_blank"><i class='bx bxl-twitter'></i></a>
-                            <a href="https://mail.google.com/mail/u/0/" target="_blank"><i class='bx bxs-envelope'></i></a>
-                            <a href="https://www.youtube.com/?gl=LK&amp;tab=r1" target="_blank"><i class='bx bxl-youtube'></i></a>
-                            <a href="https://www.instagram.com/accounts/login/" target="_blank"><i class='bx bxl-instagram'></i></a>
-                            <a href="https://www.google.com/" target="_blank"><i class='bx bxl-google-plus'></i></a>
+<!--                            <a href="https://www.facebook.com/login/" target="_blank"><i class='bx bxl-facebook'></i></a>-->
+<!--                            <a href="https://twitter.com/login" target="_blank"><i class='bx bxl-twitter'></i></a>-->
+<!--                            <a href="https://mail.google.com/mail/u/0/" target="_blank"><i class='bx bxs-envelope'></i></a>-->
+<!--                            <a href="https://www.youtube.com/?gl=LK&amp;tab=r1" target="_blank"><i class='bx bxl-youtube'></i></a>-->
+<!--                            <a href="https://www.instagram.com/accounts/login/" target="_blank"><i class='bx bxl-instagram'></i></a>-->
+<!--                            <a href="https://www.google.com/" target="_blank"><i class='bx bxl-google-plus'></i></a>-->
+<!--                       -->
                         </div>
                     </div>
 
@@ -90,8 +91,9 @@
         <div class="navbar-area">
             <!-- Menu For Mobile Device -->
             <div class="mobile-nav">
-                <a href="index.html" class="logo">
-                    <img src="assets/img/logo.png" alt="logo">
+                <a href="index.php" class="logo">
+                    <h3 style="color: white">Mecer Construction</h3>
+<!--                    <img src="assets/img/logo.png" alt="logo">-->
                 </a>
             </div>
 
@@ -99,129 +101,20 @@
             <div class="main-nav">
                 <div class="container">
                     <nav class="navbar navbar-expand-lg navbar-light">
-                        <a class="navbar-brand" href="index.html">
-                            <img src="assets/img/logo.png" alt="logo">
+                        <a class="navbar-brand" href="index.php">
+                            <h3 style="color: white">Mecer Construction</h3>
+<!--                            <img src="assets/img/logo.png" alt="logo">-->
                         </a>
                         <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
                             <ul class="navbar-nav ms-auto">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link dropdown-toggle active">Home</a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="index.html" class="nav-link active">Home One</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="index-two.html" class="nav-link">Home Two</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="index-three.html" class="nav-link">Home Three</a>
-                                        </li>
-                                    </ul>
+                                    <a href="index.php" class="nav-link">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link dropdown-toggle">Services</a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="service.html" class="nav-link">Services</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="service-two.html" class="nav-link">Services Two</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="service-details.html" class="nav-link">Services Details</a>
-                                        </li>
-                                    </ul>
+                                    <a href="about.php" class="nav-link">About Us</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link dropdown-toggle">Projects</a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="project.html" class="nav-link">Project Style One</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="project-two.html" class="nav-link">Project Style Two</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="project-three.html" class="nav-link">Project Style Three</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="project-details.html" class="nav-link">Project Details</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link dropdown-toggle">Pages</a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="about.html" class="nav-link">About Us</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="team.html" class="nav-link">Team</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="faq.html" class="nav-link">FAQ</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link dropdown-toggle">
-                                                Hover
-                                                <i class="flaticon-right-chevron"></i>
-                                            </a>
-                                            <ul class="dropdown-menu">
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link">Level 2</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link">Level 2</a>
-                                                </li>
-                                                <li class="nav-item">
-                                                    <a href="#" class="nav-link dropdown-toggle">
-                                                        Level 2
-                                                        <i class="flaticon-right-chevron"></i>
-                                                    </a>
-                                                    <ul class="dropdown-menu">
-                                                        <li class="nav-item">
-                                                            <a href="#" class="nav-link">3rd Level</a>
-                                                        </li>
-                                                        <li class="nav-item">
-                                                            <a href="#" class="nav-link">3rd Level</a>
-                                                        </li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="sign-in.html" class="nav-link">Sign In</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="sign-up.html" class="nav-link">Sign Up</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="404.html" class="nav-link">404 Page</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="testimonial.html" class="nav-link">Testimonials</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="privacy.html" class="nav-link">Privacy & Policy</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="terms.html" class="nav-link">Terms & Conditions</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link dropdown-toggle">Blog</a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="blog.html" class="nav-link">Blog Style One</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="blog-two.html" class="nav-link">Blog Style Two</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="blog-details.html" class="nav-link">Blog Details</a>
-                                        </li>
-                                    </ul>
+                                    <a href="services.php" class="nav-link">Services</a>
                                 </li>
                                 <li class="nav-item">
                                     <a href="contact.html" class="nav-link">Contact Us</a>
